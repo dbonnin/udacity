2748343b72d35b7104b1ab907485010258ca262f
Diego Jesus Bonnin Acosta <dbonnin@vision.local>
2024-01-17 18:08:34 -0300

    Added private subnets

--- cloud-devops-engineer/iac/login.bat
+++ cloud-devops-engineer/iac/login.bat
@@ -2,8 +2,7 @@ rem login.bat
 
 set PROFILE=%1
 
-aws configure set aws_access_key_id  --profile %PROFILE%
-aws configure set aws_secret_access_key  --profile %PROFILE%
-rem aws configure set aws_session_token "" --profile %PROFILE%
+aws configure set aws_access_key_id AKIA4WXSBLBEH4W2RL4Y  --profile %PROFILE%
+aws configure set aws_secret_access_key 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  --profile %PROFILE%
 aws configure set region us-east-1 --profile %PROFILE%
 aws configure set output json --profile %PROFILE%
--- cloud-devops-engineer/iac/run.bat
+++ cloud-devops-engineer/iac/run.bat
@@ -6,9 +6,25 @@ set STACK_NAME=%2
 set TEMPLATE_NAME=%3
 set PARMS_NAME=%4
 set REGION=%5
+set PROFILE=%6
+
+echo Execution with: 
+
+echo %ACTION% 
+echo %STACK_NAME%
+echo %TEMPLATE_NAME%
+echo %PARMS_NAME%
+echo %REGION%
+echo %PROFILE%
+
+
 
 if "%ACTION%"=="deploy" (
-    aws cloudformation deploy --template-file dbonnin-servers.yml --stack-name dbonnin-stack --parameter-overrides file://dbonnin-servers-parameters.json --profile udacity
+    aws cloudformation deploy --stack-name %STACK_NAME% ^
+     --template-file %TEMPLATE_NAME% --parameter-overrides file://%PARMS_NAME% --region=%REGION% --profile %PROFILE% --capabilities CAPABILITY_NAMED_IAM
+) else if "%ACTION%"=="preview" (
+    aws cloudformation deploy --stack-name %STACK_NAME% ^
+     --template-file %TEMPLATE_NAME% --parameter-overrides file://%PARMS_NAME% --region=%REGION% --profile %PROFILE% --capabilities CAPABILITY_NAMED_IAM --no-execute-changeset
 ) else if "%ACTION%"=="delete" (
     aws cloudformation delete-stack ^
     --stack-name %STACK_NAME% ^
@@ -19,5 +35,12 @@ if "%ACTION%"=="deploy" (
     --region=%REGION%
 ) else if "%ACTION%"=="list" (
     aws cloudformation list-stacks
+) else if "%ACTION%"=="validate" (
+    aws cloudformation validate-template ^
+    --template-body file://%TEMPLATE_NAME% --region=%REGION%
+) else if "%ACTION%"=="get_error" (
+    aws cloudformation describe-stack-events --stack-name %STACK_NAME% --region=%REGION% --profile %PROFILE%
+) else (
+    echo "Invalid action"
 )
 endlocal
